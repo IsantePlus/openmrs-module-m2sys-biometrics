70a3dd3af3d9eeab46930ab843d063722111aee6
Paweł Gesek <pgesek@soldevelo.com>
2017-08-08 14:10:00 +0200

    Fix logging mixin - I hope

--- api/src/main/java/org/openmrs/module/m2sysbiometrics/model/LoggingMixin.java
+++ api/src/main/java/org/openmrs/module/m2sysbiometrics/model/LoggingMixin.java
@@ -1,10 +1,12 @@
 package org.openmrs.module.m2sysbiometrics.model;
 
 import org.codehaus.jackson.annotate.JsonIgnore;
+import org.codehaus.jackson.annotate.JsonProperty;
 
 public abstract class LoggingMixin {
 
     @JsonIgnore
+    @JsonProperty("CustomerKey")
     private String customerKey;
 
     public String getCustomerKey() {
