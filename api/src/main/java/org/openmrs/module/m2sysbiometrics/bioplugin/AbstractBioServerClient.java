35d1f9f4caee75f83136d8359ccbef12c46497b7
Stanslaus <mstans8@gmail.com>
2020-12-22 17:32:23 +0300

    Update cloudscanr configurations and workflow

--- api/src/main/java/org/openmrs/module/m2sysbiometrics/bioplugin/AbstractBioServerClient.java
+++ api/src/main/java/org/openmrs/module/m2sysbiometrics/bioplugin/AbstractBioServerClient.java
@@ -7,8 +7,12 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.ws.WebServiceMessageFactory;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import org.springframework.ws.soap.SoapVersion;
+import org.springframework.ws.soap.saaj.SaajSoapMessageFactory;
 
 import javax.annotation.PostConstruct;
+import javax.xml.soap.MessageFactory;
+import javax.xml.soap.SOAPException;
 
 public abstract class AbstractBioServerClient extends WebServiceGatewaySupport implements BioServerClient {
 
@@ -21,15 +25,23 @@ public abstract class AbstractBioServerClient extends WebServiceGatewaySupport i
     @Autowired
     private M2SysProperties properties;
 
-    @Autowired
-    @Qualifier("m2sysbiometrics.messageFactory")
-    private WebServiceMessageFactory messageFactory;
+//    @Autowired
+//    @Qualifier("m2sysbiometrics.messageFactory")
+//    private WebServiceMessageFactory messageFactory;
 
     @PostConstruct
     public void init() {
         setMarshaller(marshaller);
         setUnmarshaller(marshaller);
-        setMessageFactory(messageFactory);
+//        setMessageFactory(messageFactory);
+        SaajSoapMessageFactory saajSoapMessageFactory = null;
+        try {
+            saajSoapMessageFactory = new SaajSoapMessageFactory(MessageFactory.newInstance());
+            saajSoapMessageFactory.setSoapVersion(SoapVersion.SOAP_12);
+            setMessageFactory(saajSoapMessageFactory);
+        } catch (SOAPException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
